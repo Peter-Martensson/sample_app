1314e9d235cd1c0c08f343e32ebf80a0916a95e7
Peter <amatex100@gmail.com>
2012-11-29 20:37:25 +0100

    finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -15,10 +15,16 @@ describe "Static pages" do
 		end
 
 		describe "About page" do
-		it "should have the content 'About us'" do
+		it "should have the content 'About Us'" do
 		visit '/static_pages/about/'
-		page.should have_content('About us')
+		page.should have_content('About Us')
 		end
+	end
+
+	it "should have the right title" do
+	visit '/static_pages/home'
+	page.should have_selector('title',
+										:text => "Ruby on Rails Tutorial Sample App | Home")
 	end
 end
 
